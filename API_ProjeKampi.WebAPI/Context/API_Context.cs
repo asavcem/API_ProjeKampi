cbf714d00dc793fc1418bfd4afb5ce9eaa684a35
asavcem <asavcem@gmail.com>
2025-09-05 19:21:58 +0300

    context class added.

---
+++ API_ProjeKampi.WebAPI/Context/API_Context.cs
@@ -0,0 +1,30 @@
+﻿
+using API_ProjeKampi.WebAPI.Entities;
+using System.Collections.Generic;
+
+namespace API_ProjeKampi.WebAPI.Context
+{
+    public class API_Context
+    {
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Chef> Categories { get; set; }
+
+        public DbSet<Contact> Categories { get; set; }
+
+        public DbSet<Feature> Categories { get; set; }
+
+        public DbSet<Image> Categories { get; set; }
+
+        public DbSet<Message> Categories { get; set; }
+
+        public DbSet<Product> Categories { get; set; }
+
+        public DbSet<Reservation> Categories { get; set; }
+
+        public DbSet<Service> Categories { get; set; }
+
+        public DbSet<Testimonial> Categories { get; set; }
+    }  
+}
