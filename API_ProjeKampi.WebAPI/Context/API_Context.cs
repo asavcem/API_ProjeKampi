d230b062649867d06188ad8b9e2c560839ad9a8e
asavcem <asavcem@gmail.com>
2025-09-14 20:25:57 +0300

    Migration added

--- API_ProjeKampi.WebAPI/Context/API_Context.cs
+++ API_ProjeKampi.WebAPI/Context/API_Context.cs
@@ -1,30 +1,36 @@
 ﻿
 using API_ProjeKampi.WebAPI.Entities;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_ProjeKampi.WebAPI.Context
 {
-    public class API_Context
+    public class API_Context : DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; initial catalog=ApiYummyDb; integrated security=true;");
+        }
+        
         public DbSet<Category> Categories { get; set; }
 
-        public DbSet<Chef> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
 
-        public DbSet<Contact> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
 
-        public DbSet<Feature> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
 
-        public DbSet<Image> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
 
-        public DbSet<Message> Categories { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
-        public DbSet<Product> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
-        public DbSet<Reservation> Categories { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
 
-        public DbSet<Service> Categories { get; set; }
+        public DbSet<Service> Services { get; set; }
 
-        public DbSet<Testimonial> Categories { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }  
 }
