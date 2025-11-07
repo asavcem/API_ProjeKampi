942f150a823ca79af623b1b78d15fa430ff0e15e
asavcem <asavcem@gmail.com>
2025-11-07 16:24:26 +0300

    Feature,About,Service alanları Component olarak ayarlandı. Text içeriğin alınabilinmesi için Requirements klasörü oluşturuldu

---
+++ API_ProjeKampi.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_ProjeKampi.WebUI.ViewComponents
+{
+    public class _FeatureDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ API_ProjeKampi.WebUI/ViewComponents/_ServiceDefaultCompopnentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_ProjeKampi.WebUI.ViewComponents
+{
+    public class _ServiceDefaultCompopnentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
