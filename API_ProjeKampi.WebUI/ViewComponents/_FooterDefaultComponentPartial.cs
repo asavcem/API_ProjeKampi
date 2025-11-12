f03579bf8054e5e9804933092842fb0382c198b4
asavcem <asavcem@gmail.com>
2025-11-13 00:00:34 +0300

    Footer bölümü eklendi.

---
+++ API_ProjeKampi.WebUI/ViewComponents/_FooterDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_ProjeKampi.WebUI.ViewComponents
+{
+    public class _FooterDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
